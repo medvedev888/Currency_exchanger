a861a9e60ca6b382caadd6547d0fc7459a24ce35
vladislavmedvedev <EnoT3581@mail.ru>
2024-03-22 16:02:47 +0300

    added model package

---
+++ src/main/java/me/vladislav/currency_exchanger/model/Rate.java
@@ -0,0 +1,74 @@
+package me.vladislav.currency_exchanger.model;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Rate {
+    private int id;
+    private int baseCurrencyId;
+    private int targetCurrencyId;
+    private BigDecimal rate;
+
+    public Rate(int id, int baseCurrencyId, int targetCurrencyId, BigDecimal rate){
+        this.id = id;
+        this.baseCurrencyId = baseCurrencyId;
+        this.targetCurrencyId = targetCurrencyId;
+        this.rate = rate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getBaseCurrencyId() {
+        return baseCurrencyId;
+    }
+
+    public void setBaseCurrencyId(int baseCurrencyId) {
+        this.baseCurrencyId = baseCurrencyId;
+    }
+
+    public int getTargetCurrencyId() {
+        return targetCurrencyId;
+    }
+
+    public void setTargetCurrencyId(int targetCurrencyId) {
+        this.targetCurrencyId = targetCurrencyId;
+    }
+
+    public BigDecimal getRate() {
+        return rate;
+    }
+
+    public void setRate(BigDecimal rate) {
+        this.rate = rate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Rate rate = (Rate) o;
+        return id == rate.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Rate{" +
+                "id=" + id +
+                ", baseCurrencyId=" + baseCurrencyId +
+                ", targetCurrencyId=" + targetCurrencyId +
+                ", rate=" + rate +
+                '}';
+    }
+
+}
