565ca3cd6d83baf20e14141c13b3c89117965d3d
vladislavmedvedev <EnoT3581@mail.ru>
2024-04-02 12:12:40 +0300

    fixed error in EncodingFilter

---
+++ src/main/java/me/vladislav/currency_exchanger/filters/EncodingFilter.java
@@ -0,0 +1,22 @@
+package me.vladislav.currency_exchanger.filters;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter(filterName = "EncodingFilter", value = "/*")
+public class EncodingFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        response.addHeader("Content-type", "application/json;chartset=UTF-8");
+        filterChain.doFilter(request, response);
+    }
+
+}
